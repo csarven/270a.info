dcff76800cbee2f26b69f89a34e71478c89342b0
Sarven Capadisli <info@csarven.ca>
2014-08-04 09:25:25 +0200

    Added BIS

--- scripts/get.void-linksets.sh
+++ scripts/get.void-linksets.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-rm 270a.info.void-linksets.nt
+mv 270a.info.void-linksets.nt 270a.info.void-linksets.nt.bak
 
-agencies=(uis frb fao oecd ecb imf worldbank transparency bfs);
+agencies=(uis frb fao oecd ecb imf worldbank transparency bfs bis);
 for agency in "${agencies[@]}" ; do rapper -g http://"$agency".270a.info/.well-known/void | grep linkset >> 270a.info.void-linksets.nt ; done
 
